9f57a83d1ec6aad1478dad074e7eb08f036e0a4a
Yongqiang Liu <darmenliu@gmail.com>
2024-06-16 21:37:36 +0800

    Update dependancies libraries

---
+++ vendor/github.com/tmc/langchaingo/memory/doc.go
@@ -0,0 +1,9 @@
+/*
+Package memory provides an interface for managing conversational data and
+a variety of implementations for storing and retrieving that data.
+
+The main components of this package are:
+- ChatMessageHistory: a struct that stores chat messages.
+- ConversationBuffer: a simple form of memory that remembers previous conversational back and forth directly.
+*/
+package memory
